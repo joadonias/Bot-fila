34438e5e365c88da4bfce399bcd4dc26a6db7d7e
joadonias <joadonias07@gmail.com>
2026-02-14 14:06:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
